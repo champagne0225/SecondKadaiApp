325527d03d0df90d7c9fbe56a4274c1bcd83ec30
tomoya.seki <te4090st@gmail.com>
2021-01-05 20:22:11 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 関智矢 on 2021/01/05.
+//  Copyright © 2021 tomoya.seki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
